96e7c11fb11908d9da968d13b43f39514192a3cb
a.j.fikrytan <a.j.fikrytan@sjtu.edu.cn>
2020-01-14 16:38:07 +0800

    添加了英文摘要

--- rename.sh
+++
@@ -1,4 +0,0 @@
-sed -i "s/基座装配图.png/Assembly_drawing.png/g" `grep 基座装配图.png -rl ./`
-sed -i "s/机械手装配图.png/Robot_assembly_drawing.png/g" `grep 机械手装配图.png -rl ./`
-sed -i "s/机械臂装配图.png/Robot_arm_assembly_drawing.png/g" `grep 机械臂装配图.png -rl ./`
-sed -i "s/expenditure_statistics.tex/expenditure_statistics.tex/g" `grep expenditure_statistics.tex -rl ./`
